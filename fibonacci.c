ad7bcccf090814d9cd809d177d1bfc4d7c80ecb6
BurhanMustafa-alt <mansmart418@gmail.com>
2025-08-21 10:29:28 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    int first=0,second=1,next;
+
+    printf("Enter the number of terms:");
+    scanf("%d", &n);
+
+    printf("Fibonacci series upto %d terms:\n",n);
+
+    for(i=1;i<=n;i++) {
+        if (i==1){
+            printf("%d",first);
+            continue;
+        }
+        if(i==2){
+            printf("%d",second);
+            continue;
+        }
+        next = first+second;
+        first = second;
+        second = next;
+        printf("%d",next);
+    }
+    return 0;
+}
